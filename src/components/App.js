0ef0b0bd73ba8d596421e3fa7276151691dae8db
Jyoti <jyoti.contact@gmail.com>
2020-07-21 16:32:24 +0100

    remove react styling

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import News from '.News/News'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <News />
+    </div>
+  );
+}
+
+export default App;
